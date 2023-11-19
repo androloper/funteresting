5741e6ece4d76d9b571e93c0cd02106ed14c497f
Ramazan Baybörek <61885368+androloper@users.noreply.github.com>
2023-11-19 23:34:00 +0300

    form filling bot

--- form-filling-bot/main.py
+++ form-filling-bot/main.py
@@ -1,69 +1,119 @@
 # This is a sample Python script.
 import time
+import re
+import datetime
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import locale
+# Set the locale to Russian (Russia)
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
 web = webdriver.Chrome()
-web.get('https://notumburada.com/#/kaydol')
+web.get('http://192.168.1.64/bulgaria/index.html')
 wait = WebDriverWait(web, 1)
 wait2 = WebDriverWait(web, 2)
-time.sleep(3)
+time.sleep(2)
 
-fullname = 'Deneme Hesaoasdaskdas'
-name = web.find_element('xpath', '//*[@id="fullname"]')
-name.send_keys(fullname)
+# reservation date
+dt = web.find_element('xpath', '//*[@id="bgSubmissionDocForm"]/div/h4[3]/font/font')
+date_time_str = dt.text.split(': ')[1]
+date_time = datetime.datetime.strptime(date_time_str, "%d/%m/%Y %H:%M:%S")
+formattedDt = date_time.strftime("%d.%m.%Y г. %H:%M:%S")
+print(formattedDt)  #11.09.2023 г. 16:15:00
+element = web.find_element('xpath', '//*[@id="ReservedDate"]')
+element.send_keys(formattedDt)
+# will be implemented
 
-fls = 'DH'
-firstletters = web.find_element('xpath', '//*[@id="firstLetters"]')
-firstletters.send_keys(fls)
+# name area
+usrName = 'Ramazan Baybörek'
+name = web.find_element('xpath', '//*[@id="ApplicantName"]')
+name.send_keys(usrName)
 
-uni = 0
-# university = web.find_elements('xpath', '//*[@id="mat-select-0-panel"]')
-# university_dropdown = Select(university[uni])
-# uni_xpath = f'//*[@id="mat-select-{uni}"]'
-university_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-select-0"]')))
-university_dropdown.click()
-option_xpath = f'//*[@id="mat-option-{uni}"]'
+# birth date area
+day = 11    # 10
+option_xpath = f'//*[@id="bgSubmissionDocForm"]/div/div[4]/div/select[1]/option[{day}]'
+option_element = wait.until(EC.element_to_be_clickable((By.XPATH, option_xpath)))
+option_element.click()
+month = 4 # march
+option_xpath = f'//*[@id="bgSubmissionDocForm"]/div/div[4]/div/select[2]/option[{month}]'
+option_element = wait.until(EC.element_to_be_clickable((By.XPATH, option_xpath)))
+option_element.click()
+year = 86 # 1997
+option_xpath = f'//*[@id="bgSubmissionDocForm"]/div/div[4]/div/select[3]/option[{year}]'
 option_element = wait.until(EC.element_to_be_clickable((By.XPATH, option_xpath)))
 option_element.click()
 
-dep = 4
-department_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-select-2"]')))
-department_dropdown.click()
-option_xpath = f'//*[@id="mat-option-{dep}"]'
+# passport area
+usrPassport = 'U123456789'
+passport = web.find_element('xpath', '//*[@id="ApplicantPassport"]')
+passport.send_keys(usrPassport)
+
+# passport file area
+# will be implemented
+
+# email area
+usrEmail = 'ramazanbayborek@gmail.com'
+email = web.find_element('xpath', '//*[@id="Email"]')
+email.send_keys(usrEmail)
+
+
+# citizenship area
+ctz = 208    # türkiye
+option_xpath = f'//*[@id="ResidentCountryID"]/option[{ctz}]'
 option_element = wait.until(EC.element_to_be_clickable((By.XPATH, option_xpath)))
 option_element.click()
 
-# department = web.find_elements('xpath', '//*[@id="mat-select-value-3"]')
-# department_dropdown = Select(department[dep])
-# department_dropdown.select_by_index(dep)
+# reason area
+option_xpath = '//*[@id="RegForSubmissionSubjectID"]/option[2]'
+option_element = wait.until(EC.element_to_be_clickable((By.XPATH, option_xpath)))
+option_element.click()
 
-sno = '20170601042'
-studentno = web.find_element('xpath', '//*[@id="studentNo"]')
-studentno.send_keys(sno)
+# fullname = 'Deneme Hesaoasdaskdas'
+# name = web.find_element('xpath', '//*[@id="fullname"]')
+# name.send_keys(fullname)
 
-useremail = 'deneme@deneme.com'
-email = web.find_element('xpath', '//*[@id="email"]')
-email.send_keys(useremail)
-
-userphone = '5060526793'
-phone = web.find_element('xpath', '//*[@id="phone"]')
-phone.send_keys(userphone)
-
-pwd = '123456'
-password = web.find_element('xpath', '//*[@id="password"]')
-password.send_keys(pwd)
-
-# select = web.find_element('xpath', '//*[@id="agreements"]/label/span[1]')
-aggrements = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="agreements"]/label/span[1]')))
-web.execute_script("arguments[0].scrollIntoView();", aggrements)
-aggrements.click()
+# fls = 'DH'
+# firstletters = web.find_element('xpath', '//*[@id="firstLetters"]')
+# firstletters.send_keys(fls)
+#
+# uni = 0
+# university_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-select-0"]')))
+# university_dropdown.click()
+# option_xpath = f'//*[@id="mat-option-{uni}"]'
+# option_element = wait.until(EC.element_to_be_clickable((By.XPATH, option_xpath)))
+# option_element.click()
+#
+# dep = 4
+# department_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-select-2"]')))
+# department_dropdown.click()
+# option_xpath = f'//*[@id="mat-option-{dep}"]'
+# option_element = wait.until(EC.element_to_be_clickable((By.XPATH, option_xpath)))
+# option_element.click()
+#
+# sno = '20170601042'
+# studentno = web.find_element('xpath', '//*[@id="studentNo"]')
+# studentno.send_keys(sno)
+#
+# useremail = 'deneme@deneme.com'
+# email = web.find_element('xpath', '//*[@id="email"]')
+# email.send_keys(useremail)
+#
+# userphone = '5060526793'
+# phone = web.find_element('xpath', '//*[@id="phone"]')
+# phone.send_keys(userphone)
+#
+# pwd = '123456'
+# password = web.find_element('xpath', '//*[@id="password"]')
+# password.send_keys(pwd)
+#
+# # select = web.find_element('xpath', '//*[@id="agreements"]/label/span[1]')
+# aggrements = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="agreements"]/label/span[1]')))
+# web.execute_script("arguments[0].scrollIntoView();", aggrements)
+# aggrements.click()
 
 input("Press Enter when you're ready to click the button...")
 
