e04f9e9d69e7009fda64b8b0bd5a578918b08e20
Ramazan Baybörek <61885368+androloper@users.noreply.github.com>
2025-04-20 14:13:42 +0300

    Ata attempts

---
+++ ataAttempts/main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def kisiselbilgiler():
+    # Use a breakpoint in the code line below to debug your script.
+    print("ata")
+    print("kaan")
+    print("kurubaş")
+
+
+def okulbilgileri():
+    print("abc lisesi")
+    print("abcd ilkokulu")
+
+def toplamaislerleri():
+    print(liste[0])
+    print(str(3+2**5))
+    print(type(liste))
+
+
+
+
+
+elma = "elma"
+liste = [elma,"armut"]
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    kisiselbilgiler()
+    okulbilgileri()
+    toplamaislerleri()
+
+
